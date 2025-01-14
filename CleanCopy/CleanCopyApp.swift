ee0da695e24834cd79c94ba567ea65f00c780e89
Sacha Sayan <sacha.sayan@gmail.com>
2025-01-13 20:24:20 -0500

    Initial Commit

---
+++ CleanCopy/CleanCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCopyApp.swift
+//  CleanCopy
+//
+//  Created by Sacha Sayan on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct CleanCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
