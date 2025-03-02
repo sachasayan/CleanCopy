908ccf50143b9b3604ae4bd5e861e32a5f2aa589
Sacha Sayan <sacha.sayan@gmail.com>
2025-03-02 14:22:56 -0500

    V1

---
+++ Preview Content/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanCopy
+//
+//  Created by Sacha Sayan on 2025-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
